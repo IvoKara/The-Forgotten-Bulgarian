7113010fb24a51a78b4f923349e6cc9dfe24bc2f
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-05-06 15:40:58 +0300

    Add register activity

---
+++ app/src/main/java/com/ivok/the_forgotten_bulgarian/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.ivok.the_forgotten_bulgarian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
