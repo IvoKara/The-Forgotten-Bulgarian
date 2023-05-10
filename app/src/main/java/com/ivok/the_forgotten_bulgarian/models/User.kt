aa17c1cc6cd138f0d9278b42591b1fbb95ed346a
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-05-11 00:26:55 +0300

    Raw checkpoint value check

--- app/src/main/java/com/ivok/the_forgotten_bulgarian/models/User.kt
+++ app/src/main/java/com/ivok/the_forgotten_bulgarian/models/User.kt
@@ -1,6 +1,8 @@
 package com.ivok.the_forgotten_bulgarian.models
 
+import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
+import java.util.concurrent.RecursiveTask
 
 @IgnoreExtraProperties
 data class User(
@@ -12,6 +14,18 @@ data class User(
 ) {
 }
 
+@IgnoreExtraProperties
 data class Checkpoint(var level: Int, var question: Int) {
     constructor() : this(1, 1) {}
+
+    @Exclude
+    fun isNextCheckpoint(next: Checkpoint): Boolean {
+        return (next.level == this.level && next.question == this.question + 1) ||
+                (next.level == this.level + 1 && next.question == 1)
+    }
+
+    @Exclude
+    fun isNextCheckpoint(level: Int, question: Int): Boolean {
+        return this.isNextCheckpoint((Checkpoint(level, question)))
+    }
 }
