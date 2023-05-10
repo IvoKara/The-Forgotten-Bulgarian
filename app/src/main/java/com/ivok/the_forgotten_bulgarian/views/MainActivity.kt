40d1d016f44c400f4ae88036f2fc767aa4838d54
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-05-10 17:53:16 +0300

    Hide play button when not logged in

--- app/src/main/java/com/ivok/the_forgotten_bulgarian/views/MainActivity.kt
+++ app/src/main/java/com/ivok/the_forgotten_bulgarian/views/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ivok.the_forgotten_bulgarian.views
 
 import android.content.Intent
+import android.view.View
 import com.ivok.the_forgotten_bulgarian.R
 import com.ivok.the_forgotten_bulgarian.databinding.ActivityMainBinding
 import com.ivok.the_forgotten_bulgarian.facades.AuthCompatActivity
@@ -17,6 +18,10 @@ class MainActivity : AuthCompatActivity<ActivityMainBinding>
     public override fun onStart() {
         super.onStart()
         val user = auth.currentUser
+
+        if (user == null) {
+            binding.buttonPlay.visibility = View.GONE
+        }
 
         with(binding.buttonLogin) {
             if (user != null) {
